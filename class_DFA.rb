bc42f9eea215a23b7995c763dfec633fb73bd38d
punchow12 <93099178+punchow12@users.noreply.github.com>
2026-01-27 19:15:41 -0500

    Update class_DFA.rb
    Completed function to pass only even input strings

--- class_DFA.rb
+++ class_DFA.rb
@@ -1,6 +1,40 @@
 class DFA
-	def initialize(state):
-	states
-	alphabet
-	start_state
-	accept_state;
+	def initialize(states, alphabet, transitions, start_state, accept_state):
+		@states = states
+		@alphabet = alphabet
+		@transitions = transitions
+		@current_state = start_state
+		@accept_state = accept_state
+	end
+
+	def feed(input_string)
+		input_string.chars.each do |char|
+			unless @alphabet.include?(char)
+				puts "Error Character '#{char}' not in alphabet."
+				return false
+			end
+
+			@current_state = @transitions[current_state][char]
+		end
+
+		accepted?
+	end
+
+	def accepted?
+		@accepted_states.include?(@current_state)
+	end
+end
+
+transitions = {
+	q_even:{'0' => :q_odd, '1' => :q_even},
+	q_odd: {'0' => :q_even, '1' => :q_odd}
+}
+
+dfa = DFA.new(
+	[:q_even,:q_odd],
+	['0', '1']
+	transitions,
+	:q_even,
+	[:q_even]
+	)
+puts "Testing '001': #{dfa.feed('001')}" 
